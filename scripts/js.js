e392916ec65d77fcb04c0d59e5cc994c410f4f41
carlos fernandez pradales <cfernande1408@gmail.com>
2023-03-14 17:33:42 +0100

    v3

--- scripts/js.js
+++ scripts/js.js
@@ -1,17 +1,65 @@
-function gameBoard(){
-    const rows = 3;
-    const columns = 3;
-    const board = [];
+const tablero = document.querySelector('.gameBoard');
 
-    //creating 2nd dimmension of an array Board
+/**
+ * function to organize the board of game: contains the cell information, the conditional information for the game, the symbolic representation of X and O
+ * */
 
-    for (let i=0;i<rows;i++){
-        board[i] = [];
-        for(let j = 0;j<columns;j++ ){
-            board[i].push(Cell());
-        }
+const gameBoard = (() =>{
 
+    const _rows = 3;
+    const _columns = 3;
+    const _board = [];
+
+for (let i = 0; i < _rows; i++) {
+    _board[i] = [];
+    for (let j = 0; j < _columns; j++) {
+      _board[i].push(Cell(i,j));
     }
+  }
+
+let celdaTablero = document.querySelector('.celda');
+
+return _board;
+
+})();
+
+//Cell function will cotain the information of the cells && the incorporation of the cells changes to the array
+
+function Cell(row,colum){
+   
+   var celda = document.createElement("div");
+   celda.className ="celdaTablero"+row+colum;
+
+   //moment in which the function get the parameter from the player in order to add to the cell
+
+   celda.innerHTML = " ";
+
+   tablero.appendChild(celda);
+
+};
+
+/**
+ * player selector, it contains the player logic: if it is X or O, if can or cant tic some place of cells
+ **/
+
+
+
+function player(nombreJugador, selectorJugador){
+
+    const play = ()=>{
+
+        celdaTablero 
+
+        return console.log("hey");
+
+    };
+
+
+    return {nombreJugador, selectorJugador, play}
+
 }
 
+//generación del jugador player (1 && 2), the name will be getted from the label form in the html
+
+const player1 = player(prompt("introduce tu seleccion: "), "X");
 
