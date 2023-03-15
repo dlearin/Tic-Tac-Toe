ea32908abc3d87196a7aa504fde273d1645dd856
carlos fernandez pradales <cfernande1408@gmail.com>
2023-03-15 15:38:31 +0100

    v4

--- scripts/js.js
+++ scripts/js.js
@@ -1,4 +1,4 @@
-const tablero = document.querySelector('.gameBoard');
+const tablero = document.querySelector('#gameBoard');
 
 /**
  * function to organize the board of game: contains the cell information, the conditional information for the game, the symbolic representation of X and O
@@ -10,56 +10,57 @@ const gameBoard = (() =>{
     const _columns = 3;
     const _board = [];
 
-for (let i = 0; i < _rows; i++) {
-    _board[i] = [];
-    for (let j = 0; j < _columns; j++) {
-      _board[i].push(Cell(i,j));
+    for (let i = 0; i < _rows; i++) {
+        _board[i] = [];
+        for (let j = 0; j < _columns; j++) {
+            _board[i].push(Cell(i,j));
+        }
     }
-  }
 
-let celdaTablero = document.querySelector('.celda');
+    function Cell(row,colum){
+   
+        var celda = document.createElement("div");
+        celda.className ="celdaTablero";
+        celda.id = "celda" + row + colum; // agregar un id único a la celda
 
-return _board;
+        celda.innerHTML = " ";
+
+        tablero.appendChild(celda);
+
+    };
+
+    return _board;
 
 })();
-
-//Cell function will cotain the information of the cells && the incorporation of the cells changes to the array
-
-function Cell(row,colum){
-   
-   var celda = document.createElement("div");
-   celda.className ="celdaTablero"+row+colum;
-
-   //moment in which the function get the parameter from the player in order to add to the cell
-
-   celda.innerHTML = " ";
-
-   tablero.appendChild(celda);
-
-};
 
 /**
  * player selector, it contains the player logic: if it is X or O, if can or cant tic some place of cells
  **/
 
-
-
 function player(nombreJugador, selectorJugador){
 
-    const play = ()=>{
+    const play = (()=>{
 
-        celdaTablero 
+        const celdas = document.querySelectorAll('#gameBoard .celdaTablero'); // seleccionar todas las celdas del tablero
 
-        return console.log("hey");
+        celdas.forEach((celda) => { // agregar un addEventListener a cada celda
+            celda.addEventListener('click',function(){
 
-    };
+                if (selectorJugador == "X"){
+                    celda.innerHTML = "X";
+                }
+    
+                if (selectorJugador=="O"){
+                    celda.innerHTML = "O";
+                }
+            })
+        });
 
+    })();
 
     return {nombreJugador, selectorJugador, play}
-
 }
 
 //generación del jugador player (1 && 2), the name will be getted from the label form in the html
 
-const player1 = player(prompt("introduce tu seleccion: "), "X");
-
+const player1 = new player(prompt("introduce tu seleccion: "), "X");
